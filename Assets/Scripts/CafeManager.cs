a73abd569f6f7067f5a427160812dfda322b47c5
Nick Stokowski <nkstokowski@gmail.com>
2025-02-17 23:09:36 -0500

    Adding images to shop

--- Assets/Scripts/CafeManager.cs
+++ Assets/Scripts/CafeManager.cs
@@ -13,11 +13,11 @@ public class CafeManager : MonoBehaviour, IDataPersistence
     public List<MenuItem> allMenuItems;
     private Dictionary<String, MenuItem> menuItemsDict;
 
-    Vector3 upperGuestPos = new Vector3(-1.65999997f,-0.349999994f,0f);
-    Vector3 lowerGuestPos = new Vector3(1.87f,-2.38000011f,0f);
+    Vector3 upperGuestPos = new Vector3(-1.65999997f,-0.42f,-1f);
+    Vector3 lowerGuestPos = new Vector3(1.87f,-2.38000011f,-1f);
 
-    Vector3 upperDrinkPos = new Vector3(-1.06900001f,-0.757000029f,0f);
-    Vector3 lowerDrinkPos = new Vector3(1.31700003f,-2.773f,0f);
+    Vector3 upperDrinkPos = new Vector3(-1.06900001f,-0.757000029f,-1f);
+    Vector3 lowerDrinkPos = new Vector3(1.31700003f,-2.773f,-2f);
 
     DateTime timeLastSaved;
 
